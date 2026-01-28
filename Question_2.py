b627c6f391e61996da7274676149fe0ab63bf329
khanhduynguyen2301-collab · <khanhduy.nguyen2301@gmail.com>
2026-01-28 02:58:34 -0700

    Describe what you changed

--- Question_2.py
+++
@@ -1,9 +0,0 @@
-def nested_dictionary(string):
-    dictionary = {}
-    for item in string.split():
-        dictionary[item] = {'length': len(item),
-                            'parity': 'even' if len(item) % 2 == 0 else 'odd'}
-    return dictionary
-
-string = 'data science'
-print(nested_dictionary(string))
